12e1760c47ad361c8b39a1089ab3da0b95bf854c
rannasameh <rannasameh@hotmail.com>
2024-05-15 13:34:41 +0300

    Common components to reuse among components

---
+++ src/commonComponents/CustomTabs/CustomTabs.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import CustomTabPanel from "../../commonComponents/CustomTabPanel/CustomtabPanel";
+
+const CustomTabs = ({ tabComponents }) => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs value={value} onChange={handleChange}>
+          {Object.keys(tabComponents).map((label, index) => (
+            <Tab key={index} label={label} />
+          ))}
+        </Tabs>
+      </Box>
+      {Object.entries(tabComponents).map(([label, component], index) => (
+        <CustomTabPanel key={index} value={value} index={index}>
+          {component}
+        </CustomTabPanel>
+      ))}
+    </Box>
+  );
+};
+
+export default CustomTabs;
---
+++ src/commonComponents/SortingTable/Components/SortingTableHead/SortingTableHead.js
@@ -0,0 +1,42 @@
+import {
+  TableRow,
+  TableHead,
+  TableCell,
+  TableSortLabel,
+  Box,
+} from "@mui/material";
+import { visuallyHidden } from "@mui/utils";
+
+const SortingTableHead = ({ headCells, order, orderBy, onRequestSort }) => {
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align="right"
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
+export default SortingTableHead;
